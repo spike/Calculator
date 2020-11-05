8f7772daf9da753d833c49044411eb6451932454
Stephan Branczyk <stephan@branczyk.org>
2020-11-04 16:33:28 -0800

    added decimal function

---
+++ app/src/main/java/com/calculator/calc/Calculation.kt
@@ -0,0 +1,18 @@
+package com.calculator.calc
+
+class Calculation {
+    fun addNumber(): String {
+        return ""
+    }
+    fun clear(): String {
+        return ""
+    }
+
+    fun compute(operandOne: String, operandTwo: String, operatorSign: String): String {
+        return ""
+    }
+    fun delete(): String {
+        return ""
+    }
+}
+
