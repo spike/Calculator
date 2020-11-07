7c8ccfcf7a5efb1a17bec9beabbb55b2a5d2fa01
Stephan Branczyk <stephan@branczyk.org>
2020-11-06 16:52:15 -0800

    added some more thorough unit tests to ferret out all the edge cases

--- app/src/main/java/com/calculator/calc/Calculation.kt
+++ app/src/main/java/com/calculator/calc/Calculation.kt
@@ -1,10 +1,16 @@
 package com.calculator.calc
 
 class Calculation {
-
+    // val aaa ="12345+5="
+    // val bbb = listOf("12345", "5", "12350")
     fun calculate(a: String): List<String> {
         var result: MutableList<String> = mutableListOf()
         for (e in a) {
+          //  if (e.equals())
+            // isSign
+            // isDot
+            // isNumber
+
 
         }
         return result
