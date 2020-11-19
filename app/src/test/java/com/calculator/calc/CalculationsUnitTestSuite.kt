bd6d4cb787879632129f9d9d8485e6da5bd50d83
Stephan Branczyk <stephan@branczyk.org>
2020-11-19 15:30:26 -0800

    fixed the unit tests

--- app/src/test/java/com/calculator/calc/CalculationsUnitTestSuite.kt
+++ app/src/test/java/com/calculator/calc/CalculationsUnitTestSuite.kt
@@ -9,56 +9,51 @@ import java.math.BigDecimal
 class CalculationsUnitTestSuite {
     private lateinit var c: Calculation
 
-
-
-    @Test
-    fun testCalculateEasyCase() {
-        val a = "2*3="
-        val b = listOf("2", "*", "3", "6")
-        assertEquals(b, c.calculate(a))
-    }
-
-    @Test
-    fun testCalculateFirstCase() {
-        val a = "0567b+2.56-.5=="
-        val b = listOf("0", "5", "56", "567", "56", "+", "2", "2.", "2.5", "2.56",
-            "(58.56)-", "0.", "0.5", "58.06", "58.06" )
-        assertEquals(b, c.calculate(a))
-    }
-
-    val aa = "1..3.2+0.5+78*1-.82="
-    val bb = listOf("1", "1.", "1.", "1.3", "1.3", "1.32", "+", "0", "0.",
-        "0.5", "(1.82)+", "7", "78", "(79.82)*", "1", "(79.82)-",
-        "0.", "0.8", "0.82", "79")
-    @Test
-    fun testCalculateSecondCase() {
-        assertEquals(bb, c.calculate(aa))
-    }
-
-    val aaa ="12345+5="
-    val bbb = listOf("1", "12", "123", "1234", "12345", "+", "5", "12350")
-    @Test
-    fun testCalculateThirdCase() {
-        assertEquals(bbb, c.calculate(aaa))
-    }
-
-    val aaaa = "3+*2=="
-    val bbbb = listOf("3", "+", "*", "2", "6", "6")
-    @Test
-    fun testCalculateFourthCase() {
-        assertEquals(bbbb, c.calculate(aaaa))
-    }
-
-    val aaaaa = "37/100="
-    val bbbbb = listOf("3", "37", "/", "1", "10", "100", "0.37")
-    @Test
-    fun testRoundingError() {
-        assertEquals(bbbbb, c.calculate(aaaaa))
-    }
     @Before
     fun setUp(): Unit {
         c = Calculation()
     }
+
+    @Test
+    fun testCalculateEasyCase() {
+        val input = "2*3="
+        val expected = listOf("2", "*", "3", "6")
+        assertEquals(expected, c.calculate(input))
+    }
+    @Test
+    fun testCalculateFirstCase() {
+        val input = "0567b+2.56-.5=="
+        val expected = listOf("0", "5", "56", "567", "56", "+", "2", "2.", "2.5", "2.56",
+            "(58.56)-", "0.", "0.5", "58.06", "58.06" )
+        assertEquals(expected, c.calculate(input))
+    }
+    @Test
+    fun testCalculateSecondCase() {
+        val input = "1..3.2+0.5+78*1-.82="
+        val expected = listOf("1", "1.", "1.", "1.3", "1.3", "1.32", "+", "0", "0.",
+            "0.5", "(1.82)+", "7", "78", "(79.82)*", "1", "(79.82)-",
+            "0.", "0.8", "0.82", "79")
+        assertEquals(expected, c.calculate(input))
+    }
+    @Test
+    fun testCalculateThirdCase() {
+        val input ="12345+5="
+        val expected = listOf("1", "12", "123", "1234", "12345", "+", "5", "12350")
+        assertEquals(expected, c.calculate(input))
+    }
+    @Test
+    fun testCalculateFourthCase() {
+        val input = "3+*2=="
+        val expected = listOf("3", "+", "*", "2", "6", "6")
+        assertEquals(expected, c.calculate(input))
+    }
+    @Test
+    fun testRoundingError() {
+        val input = "37/100="
+        val expected = listOf("3", "37", "/", "1", "10", "100", "0.37")
+        assertEquals(expected, c.calculate(input))
+    }
+
     @After
     fun tearDown() {
         c.clear()
