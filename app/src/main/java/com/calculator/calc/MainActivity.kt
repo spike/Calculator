5b76103bdbf40a5af06e36917fd0fffe8ebcdd0d
Stephan Branczyk <stephan@branczyk.org>
2020-11-22 19:31:43 -0800

    trying new layout

--- app/src/main/java/com/calculator/calc/MainActivity.kt
+++ app/src/main/java/com/calculator/calc/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_material_design)
     }
 
     fun operand(v: View) {
