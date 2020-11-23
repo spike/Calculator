ca95634908acf6a6e951910ee4a42febfbc18756
Stephan Branczyk <stephan@branczyk.org>
2020-11-23 00:13:34 -0800

    trying new layout

--- app/src/main/java/com/calculator/calc/MainActivity.kt
+++ app/src/main/java/com/calculator/calc/MainActivity.kt
@@ -13,7 +13,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_material_design)
+        setContentView(R.layout.activity_main_material_design2)
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
     }
 
     fun operand(v: View) {
